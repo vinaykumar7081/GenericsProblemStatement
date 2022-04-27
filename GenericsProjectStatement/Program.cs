ec7102e0e49909bc3a8c17dc35d73985a1d1717b
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-27 15:20:38 +0530

    [VINAY] Add: Display Maximum Integer Number

---
+++ GenericsProjectStatement/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using GenericsProjectStatement;
+class Program
+{
+    public static void Main(String[] args)
+    { 
+    ArrayInteger arr=new ArrayInteger();
+        int[] intArr = { 1, 2, 4, 587, 54, 78, 9547, 1254 };
+        arr.MaximumElement(intArr);
+    }
+}
